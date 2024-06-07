e6631dc9ce2c0a068e066e70214f86e3fe9660f1
adam.brix <adam.brix@student.howest.be>
2024-06-07 13:20:15 +0200

    start of arrivals plugin

--- plugins/app/arrivals/models/Arrival.php
+++ plugins/app/arrivals/models/Arrival.php
@@ -3,72 +3,27 @@
 use Model;
 
 /**
- * Arrival Model
+ * Model
  */
 class Arrival extends Model
 {
     use \October\Rain\Database\Traits\Validation;
+    
+    /*
+     * Disable timestamps by default.
+     * Remove this line if timestamps are defined in the database table.
+     */
+    public $timestamps = false;
+
 
     /**
-     * @var string table associated with the model
+     * @var string The database table used by the model.
      */
-    public $table = 'app_arrivals_arrivals';
+    public $table = 'app_arrivals_';
 
     /**
-     * @var array guarded attributes aren't mass assignable
+     * @var array Validation rules
      */
-    protected $guarded = ['*'];
-
-    /**
-     * @var array fillable attributes are mass assignable
-     */
-    protected $fillable = [];
-
-    /**
-     * @var array rules for validation
-     */
-    public $rules = [];
-
-    /**
-     * @var array Attributes to be cast to native types
-     */
-    protected $casts = [
-        'arrival_time' => 'datetime'
+    public $rules = [
     ];
-
-    /**
-     * @var array jsonable attribute names that are json encoded and decoded from the database
-     */
-    protected $jsonable = [];
-
-    /**
-     * @var array appends attributes to the API representation of the model (ex. toArray())
-     */
-    protected $appends = [];
-
-    /**
-     * @var array hidden attributes removed from the API representation of the model (ex. toArray())
-     */
-    protected $hidden = [];
-
-    /**
-     * @var array dates attributes that should be mutated to dates
-     */
-    protected $dates = [
-        'created_at',
-        'updated_at'
-    ];
-
-    /**
-     * @var array hasOne and other relations
-     */
-    public $hasOne = [];
-    public $hasMany = [];
-    public $belongsTo = [];
-    public $belongsToMany = [];
-    public $morphTo = [];
-    public $morphOne = [];
-    public $morphMany = [];
-    public $attachOne = [];
-    public $attachMany = [];
 }
